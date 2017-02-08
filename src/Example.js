31574876e401f59036e849e72a091ba10e08a450
Grace Han <hanpanpan200@gmail.com>
2017-02-08 16:01:48 +0800

    feat: add Example component

---
+++ src/Example.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+} from 'react-native'
+
+export default class Example extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}
